c8d6d88758d5dae5abaf96faa4939cde986ef8f9
Aritra Roy <aritra.roy.in@gmail.com>
2017-02-19 17:51:27 +0530

    Several UI enhancements

---
+++ app/src/main/java/com/aritraroy/rxmagnetodemo/FeaturesConfig.java
@@ -0,0 +1,19 @@
+package com.aritraroy.rxmagnetodemo;
+
+/**
+ * Created by aritraroy on 19/02/17.
+ */
+
+public class FeaturesConfig {
+
+    public static final int FEATURE_URL = 1;
+    public static final int FEATURE_VERSION = 2;
+    public static final int FEATURE_IS_UPDATE_AVAILABLE = 3;
+    public static final int FEATURE_DOWNLOADS = 4;
+    public static final int FEATURE_PUBLISHED_DATE = 5;
+    public static final int FEATURE_OS_REQUIREMENTS = 6;
+    public static final int FEATURE_CONTENT_RATING = 7;
+    public static final int FEATURE_APP_RATING = 8;
+    public static final int FEATURE_APP_RATINGS_COUNT = 9;
+    public static final int FEATURE_CHANGELOG = 10;
+}
--- app/src/main/java/com/aritraroy/rxmagnetodemo/MainActivity.java
+++ app/src/main/java/com/aritraroy/rxmagnetodemo/MainActivity.java
@@ -1,12 +1,20 @@
 package com.aritraroy.rxmagnetodemo;
 
+import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.view.View;
+import android.support.v7.widget.SimpleItemAnimator;
+import android.text.TextUtils;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.aritraroy.rxmagneto.RxMagneto;
@@ -15,14 +23,16 @@ import com.aritraroy.rxmagnetodemo.domain.FeatureModel;
 import java.util.ArrayList;
 import java.util.List;
 
-import rx.Subscriber;
+import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView mLogo;
     private RecyclerView mFeatureRecycler;
     private Button mGrabResultButton;
+    private EditText mPackageNameEditText;
 
     private List<FeatureModel> mFeatureModelList;
     private FeaturesRecyclerAdapter mFeatureModelRecyclerAdapter;
@@ -35,32 +45,54 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.setStatusBarColor(ContextCompat.getColor(this, R.color.mine_shaft));
+            window.setNavigationBarColor(ContextCompat.getColor(this, R.color.mine_shaft));
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+
         rxMagneto = RxMagneto.getInstance();
         rxMagneto.initialize(this);
 
+        mLogo = (ImageView) findViewById(R.id.logo);
         mFeatureRecycler = (RecyclerView) findViewById(R.id.features_recycler);
         mGrabResultButton = (Button) findViewById(R.id.grab_result);
+        mPackageNameEditText = (EditText) findViewById(R.id.package_name);
 
         mFeatureModelList = getFeatureModelsList();
         mFeatureModelRecyclerAdapter = new FeaturesRecyclerAdapter(mFeatureModelList);
         mFeatureRecycler.setLayoutManager(new LinearLayoutManager(this));
+        mFeatureRecycler.setWillNotDraw(false);
         mFeatureRecycler.setAdapter(mFeatureModelRecyclerAdapter);
-        mFeatureModelRecyclerAdapter.setOnClickListener(new FeaturesRecyclerAdapter.OnClickListener() {
-            @Override
-            public void onClick(int featureId) {
-                mCurrentSelectedFeatureId = featureId;
-                Log.d("TAG", "SELECTED: " + mCurrentSelectedFeatureId);
+
+        // Removing all RecycleView animations
+        ((SimpleItemAnimator) mFeatureRecycler.getItemAnimator()).setSupportsChangeAnimations(false);
+
+        mFeatureModelRecyclerAdapter.setOnClickListener(featureId -> {
+            mCurrentSelectedFeatureId = featureId;
+        });
+
+        mGrabResultButton.setOnClickListener(v -> {
+            String packageName = mPackageNameEditText.getText().toString();
+
+            if (TextUtils.isEmpty(packageName)) {
+                Toast.makeText(MainActivity.this,
+                        getResources().getString(R.string.message_package_name_not_empty),
+                        Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            if (mCurrentSelectedFeatureId == 0) {
+                getPlayStoreUrl(packageName);
             }
         });
 
-        mGrabResultButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mCurrentSelectedFeatureId == 0) {
-                    getPlayStoreUrl();
-                }
-            }
-        });
+        // Sample package name. Can be changed to any name
+        mPackageNameEditText.setText("com.codexapps.andrognito");
+        mPackageNameEditText.clearFocus();
+        mLogo.requestFocus();
     }
 
     private List<FeatureModel> getFeatureModelsList() {
@@ -75,25 +107,16 @@ public class MainActivity extends AppCompatActivity {
         return featureModelsList;
     }
 
-    private void getPlayStoreUrl() {
-        rxMagneto.grabUrl()
-                .subscribeOn(Schedulers.immediate())
+    private void getPlayStoreUrl(String packageName) {
+        Observable<String> urlObservable = rxMagneto.grabUrl(packageName);
+
+        urlObservable.subscribeOn(Schedulers.immediate())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Subscriber<String>() {
-                    @Override
-                    public void onCompleted() {
+                .subscribe(s -> {
+                    showResult(mFeatureModelList.get(FeaturesConfig.FEATURE_URL).getTitle(), s);
+                }, throwable -> {
+                    showResult(getResources().getString(R.string.label_error), throwable.getMessage());
 
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-
-                    }
-
-                    @Override
-                    public void onNext(String s) {
-                        showResult(mFeatureModelList.get(0).getTitle(), s);
-                    }
                 });
     }
 
@@ -102,6 +125,14 @@ public class MainActivity extends AppCompatActivity {
                 .title(title)
                 .content(result)
                 .positiveText(R.string.label_ok)
+                .show();
+    }
+
+    public static MaterialDialog showLoading(Activity activity, String message) {
+        return new MaterialDialog.Builder(activity)
+                .content(message)
+                .cancelable(false)
+                .progress(true, 0)
                 .show();
     }
 }
